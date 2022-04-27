0e155b1aa44c3edbbc441ae54c5dc31cf7c56da9
Ranmond JIN <cri.sri@outlook.com>
2022-04-27 23:06:38 +0800

    [feat] 增加typeScript支持 (@rollup/plugin-typescript) 生成声明文件, TODO: 删除@babel/preset-typescript

--- rollup.config.js
+++ rollup.config.js
@@ -1,25 +1,28 @@
-import { nodeResolve } from '@rollup/plugin-node-resolve';
+import {nodeResolve} from '@rollup/plugin-node-resolve';
 import commonjs from '@rollup/plugin-commonjs';
-import babel  from '@rollup/plugin-babel';
-
+import babel from '@rollup/plugin-babel';
+import typescript from "@rollup/plugin-typescript";
 
 
 export default {
     input: "src/index.js",
+    // acornInjectPlugins: [jsx()],
+
     plugins: [
         nodeResolve({
             extensions: ['.js', '.jsx', '.ts', '.tsx']
         }),
         commonjs({include: "node_modules/**"}),
-        babel({
-            babelHelpers: 'bundled',
-            exclude: 'node_modules/**',
-            extensions: ['.js', '.jsx', '.ts', '.tsx']
-        }),
+        typescript({ tsconfig: './tsconfig.json' }),
+        // babel({
+        //     babelHelpers: 'bundled',
+        //     exclude: 'node_modules/**',
+        //     // extensions: ['.js', '.jsx', '.ts', '.tsx']
+        // }),
 
     ],
     output: {
-        file: 'dist/bundle.js',
+        file: 'dist/index.js',
         format: 'esm',
     },
     external: ['react']
--- src/button.tsx
+++ src/button.tsx
@@ -1,5 +1,4 @@
 import React from 'react';
-
 interface haha {
     child: void;
     data: Array<string>;
